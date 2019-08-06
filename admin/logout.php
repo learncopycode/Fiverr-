a47de772759154fb5cdb7eae38dab954bd94b5fb
Shomoy <dhushorshomoy95@gmail.com>
2019-08-06 19:13:17 +0600

    asd

--- admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo '<script>alert("Logout Success");window.location.assign("index.php");</script>';
-?>
--- php-includes/check-login.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['id']) && $_SESSION['login_type']=='user'){
-}
-else{
-	echo '<script>alert("Access denied");window.location.assign("index.php");</script>';
-}
-?>
